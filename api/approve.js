864582fe38a82d0ad384265b6f6d4ce6fdc7e4b3
dogan08ay <doganayfd08@gmail.com>
2026-06-15 00:15:12 +0300

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,77 +1,10 @@
+// api/approve.js
 export default async function handler(req, res) {
-
-    if (req.method !== 'POST') {
-
-        return res.status(405).json({
-            error: 'Method not allowed'
-        });
-
-    }
-
-    try {
-
-        const {
-            paymentId,
-            action,
-            txid
-        } = req.body;
-
-        let url = '';
-
-        let body = {};
-
-        if (action === 'approve') {
-
-            url = `https://api.minepi.com/v2/payments/${paymentId}/approve`;
-
-        }
-
-        if (action === 'complete') {
-
-            url = `https://api.minepi.com/v2/payments/${paymentId}/complete`;
-
-            body = {
-                txid: txid
-            };
-
-        }
-
-        const response = await fetch(
-
-            url,
-
-            {
-
-                method: 'POST',
-
-                headers: {
-
-                    Authorization: `Key ${process.env.PI_API_KEY}`,
-
-                    'Content-Type': 'application/json'
-
-                },
-
-                body: action === 'complete'
-                    ? JSON.stringify(body)
-                    : undefined
-
-            }
-
-        );
-
-        const data = await response.json();
-
-        return res.status(200).json(data);
-
-    } catch (error) {
-
-        return res.status(500).json({
-
-            error: error.message
-
-        });
-
-    }
-
+  if (req.method === 'POST') {
+    // Burada Pi Network API ile haberleşmen gerekiyor
+    // AppSecret anahtarını kontrol et
+    res.status(200).json({ success: true });
+  } else {
+    res.status(405).end();
+  }
 }
